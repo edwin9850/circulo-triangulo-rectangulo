e8890f717ff56d0e51d29d8fdc3dc824e7ad7bec
edwin pardo <124716322+edwin9850@users.noreply.github.com>
2023-03-31 20:26:53 +0000

    programación orientada a objetos

--- main.cpp
+++ main.cpp
@@ -18,20 +18,23 @@ float perimetro() {return 2 * 3.1416 * radio; }
 float diametro() {return 2 * radio;}
 };
 
+/* Clase triangulo rectangulo para el cálculo de: hipotenusa, perimetro y altura*/
 class triangulo_rectangulo{
 
+// Datos miembros PRIVADO
 private:
 float cateto_a;
 float cateto_b;
 float cateto_c;
 
+ // El constructor, para pasar datos iniciales al objeto al ser instanciado
 public: 
 triangulo_rectangulo(float ca, float cb, float cc){
   cateto_a=ca;
   cateto_b=cb;
   cateto_c=cc;
 }
-
+ // Métodos miembros de la clase
 float hipotenusa(){
  
 cateto_c=(cateto_a*cateto_a)+(cateto_b*cateto_b);
@@ -50,15 +53,17 @@ float altura (){ return (cateto_a*cateto_b)/cateto_c;}
 
 int main() {
   float r;
-  /*Se juega con la clase Circulo*/
+  //Se juega con la clase Circulo
   cout << "        Bienvenido       "<<endl<<endl;
   
 cout<<"            circulo            "<<endl<<endl;
   cout<<" ingrese el radio: ";
   cin >> r;
   cout<<endl;
-  /*Se instancia el objeto circulo*/
+  //Se instancia el objeto circulo
   Circulo circ(r);
+
+  //se muestra los metodos de la clase
   cout << "el area del circulo es: "<<circ.area()<<endl;
   cout << "el perimetro del circulo es: "<<circ.perimetro()<<endl;
  cout << "el diametro del circulo es: "<<circ.diametro()<<endl;
@@ -77,8 +82,10 @@ cout<<"ingrese la medida del cateto b: ";
   cin>>b;
   cout<<endl;
   
+  /*Se instancia el objeto circulo*/
 triangulo_rectangulo t_r(a,b,c);
 
+  //se muestra los metodos de la clase
   cout<<"la hipotenusa del triangulo rectangulo es: "<<t_r.hipotenusa( )<<endl;
   cout << "el perimetro del triangulo rectangulo es: "<<t_r.perimetro()<<endl;
     cout << "la altura del triangulo rectangulo es: "<<t_r.altura()<<endl;
